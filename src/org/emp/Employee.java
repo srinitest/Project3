c1dc4919495c8fc0c751e4c774b6a2d309a25bc0
srinitest <yuvaraghavan87@gmail.com>
2020-12-18 14:56:35 +0530

    task

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,21 @@
+package org.emp;
+
+public class Employee {
+public static void main(String[] args) {
+	int empid=121344;
+	System.out.println("Emp ID:"+empid);
+	String empname="Vijay";
+	System.out.println("EmpName:"+empname);
+	String empemailid="vjvijayo73@gmail.com";
+	System.out.println("Emp Email ID:"+empemailid);
+	long empphno=9865005446l;
+	System.out.println("Emp PhNO:"+empphno);
+	float empsalary=76544.4343f;
+	System.out.println("Emp Salary:"+empsalary);
+	char gen=('M');
+	System.out.println("Emp Gender:"+gen);
+	String city="Karaikudi";
+	System.out.println("Emp City:"+city);
+	
+}
+}
